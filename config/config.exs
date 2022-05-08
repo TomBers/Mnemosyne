96e9eaa9f136da591493cd784e9f3151ec799dfa
TomBers <tjcberman@gmail.com>
2022-05-08 21:12:43 +0100

    working alpha

--- config/config.exs
+++ config/config.exs
@@ -60,6 +60,7 @@ config :crawly,
     {Crawly.Pipelines.WriteToFile, extension: "json", folder: "/Users/tomberman/Development/mnemosyne/logs"},
   ]
 
+#  TODO - reenable the crawling
 config :mnemosyne, Mnemosyne.Scheduler,
   jobs: [
 #    {"* * * * *",      {Mnemosyne.Scheduler, :run_every_min, []}},
