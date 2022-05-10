e3b1ec45dd1b4ad6f6c1d57f7860d4ea5d2733fa
TomBers <tjcberman@gmail.com>
2022-05-10 15:54:54 +0100

    fix when not using scraper as source

--- lib/mnemosyne_web/views/source_view.ex
+++ lib/mnemosyne_web/views/source_view.ex
@@ -1,3 +1,12 @@
 defmodule MnemosyneWeb.SourceView do
   use MnemosyneWeb, :view
+
+  def get_page_elements(nil) do
+    []
+  end
+
+  def get_page_elements(elements) do
+    elements
+  end
+
 end
