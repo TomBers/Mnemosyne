f3ac7b884fd0cf1193af3bedf5d1d27f640f8c92
TomBers <tjcberman@gmail.com>
2022-05-10 14:25:44 +0100

    a bit of cleanup

--- lib/mnemosyne/apis/api_from_source.ex
+++ lib/mnemosyne/apis/api_from_source.ex
@@ -4,7 +4,19 @@ defmodule ApiFromSource do
 
   def run_sources(sources) do
     sources
-    |> Enum.map(fn source -> apply(String.to_atom("Elixir.#{source.url}"), :run, [source]) end)
-    |> Enum.map(fn {response, source} -> Records.create_snapshot(%{url: source.url, type: "API", response: response, source_id: source.id, html: Jason.encode!(response)}) end)
+    |> Enum.map(fn source -> call_module_from_source(source) end)
+    |> Enum.map(fn {response, source} -> store_snapshot(response, source) end)
   end
+
+  def call_module_from_source(source) do
+#    This is a little Ugly!
+    module_name = String.to_atom("Elixir.#{source.url}")
+    apply(module_name, :run, [source])
+  end
+
+  def store_snapshot(response, source) do
+    Records.create_snapshot(%{url: source.url, type: "API", response: response, source_id: source.id, html: Jason.encode!(response)})
+  end
+
+
 end
